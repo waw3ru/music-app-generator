02db081b36718d4383c44baaafcadefed90b4f54
John ⚒  Waw3ru <waweruj00@gmail.com>
2025-04-03 07:21:03 +0300

    chore: Remove unnecessary env details

--- api/typings.d.ts
+++ api/typings.d.ts
@@ -1,8 +1,5 @@
 declare module 'bun' {
   interface Env {
-    SUPABASE_SERVICE_ROLE: string;
-    SUPABASE_PROJECT_URL: string;
-    SUPABASE_PROJECT_ID: string;
     CLIENT_URL: string;
     PORT: string;
   }
