8a6a9e9c1d73f69437d1c85f9750c5989e75b68b
John ⚒  Waw3ru <waweruj00@gmail.com>
2025-04-23 10:17:30 +0300

    fix: crude first page design

---
+++ app/components/UnsignedMusicSelector.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+<div :class="$style['selector']">
+  <InputText type="text" size="large" style="--p-inputtext-lg-padding-y: 1rem; width: 100%" />
+  <Button label="Create" icon="pi pi-user" size="large" style="margin-inline: 1rem" />
+</div>
+</template>
+
+<style lang="scss" module scoped>
+.selector {
+  width: 40.25rem;
+  border: 1px solid white;
+  padding: 1.75rem 0.75rem;
+  border-radius: 0.75rem;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+</style>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,21 +1,42 @@
 <script setup lang="ts">
-
+// const typewriterTexts = ref([
+//   'Create a love song about how beautiful you are.',
+//   'Create a hiphop song about how to work hard',
+//   'Create a summer song about an amazing day at the beach',
+// ]);
+//
+// const loopTypewriterText = () => {
+//
+// }
 </script>
 
 <template>
 <div :class="$style.page">
   <div :class="$style['page-topbar']">
-    <img src="~/assets/images/logo.svg" alt="Logo">
+    <img src="~/assets/images/logo.svg" alt="Logo" :class="$style['logo']" >
 
     <div :class="$style['page-action-buttons']">
-      you
+      <Button label="Sign In" variant="outlined" size="large" style="margin-inline: 0.625rem" />
+      <Button label="Sign Up" size="large" />
+    </div>
+  </div>
+
+  <!-- page content -->
+  <div :class="$style['page-content']">
+    <div :class="$style['typewriter']">
+      <h1>Create a love song about how beautiful you are.</h1>
+      <p>Start with a simple prompt or dive into our pro editing tools, your next track is just a step away.</p>
+    </div>
+
+    <div :class="$style['page-prompt']">
+      <UnsignedMusicSelector />
     </div>
   </div>
 </div>
 </template>
 
 <style lang="scss" module scoped>
-$topbar-height: 1.875rem;
+$topbar-height: 4.875rem;
 
 .page {
   width: 100%;
@@ -31,10 +52,67 @@ $topbar-height: 1.875rem;
   background: transparent;
   display: flex;
   justify-content: space-between;
-  padding: 0.625rem;
+  align-items: center;
+  padding: 0.625rem 1rem;
 }
 
-.page-action-buttons {
+.logo {
+  width: auto;
+  height: 50px;
+}
 
+.page-content {
+  background: transparent;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-wrap: wrap;
+  padding: 0.625rem 1rem;
+}
+
+.typewriter {
+  max-width: 54.688rem;
+  height: auto;
+  flex-shrink: 0;
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+
+  h1 {
+    height: auto;
+    overflow: hidden; /* Ensures the content is not revealed until the animation */
+    margin: 0 auto; /* Gives that scrolling effect as the typing happens */
+    //letter-spacing: .15em; /* Adjust as needed */
+    //animation: typing 3.5s steps(40, end) infinite;
+    font-size: 3.5rem;
+    text-align: center
+  }
+
+  p {
+    max-width: 34.688rem;
+    font-size: 1rem;
+    text-align: center;
+    margin-block-start: 4rem;
+  }
+}
+
+.page-prompt {
+  width: 100%;
+  flex-shrink: 0;
+  display: flex;
+  justify-content: center;
+  margin-block-start: 4rem;
+}
+
+/* The typing effect */
+@keyframes typing {
+  from { width: 0 }
+  to { width: 100% }
+}
+
+/* The typewriter cursor effect */
+@keyframes blink-caret {
+  from, to { border-color: transparent }
+  50% { border-color: orange; }
 }
 </style>
